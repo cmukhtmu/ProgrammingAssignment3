8abd97265a40a75d9ef72fd79392668c1e5795ed
Mukarram Mukhtar <cmukhtmu@jci.com>
2019-01-07 21:26:35 -0600

    first commit

---
+++ best.R
@@ -0,0 +1,20 @@
+best <- function(state, outcome) {
+    ## Read outcome data
+    outcomeData = read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    
+    ## Check that state and outcome are valid
+    if(!(state %in% outcomeData$State)) stop("invalid state")
+
+    if(!(outcome %in% c("heart attack","heart failure","pneumonia"))) stop("invalid outcome")
+    
+    print("Input looks good")
+    
+    ## Return hospital name in that state with lowest 30-day death
+    if(outcome == "heart attack") outcomeCol = 11
+    else if(outcome == "heart failure") outcomeCol = 17
+    else if(outcome == "pneumonia") outcomeCol = 23
+    outcomeData = subset(outcomeData, State==state)
+    min(as.numeric(outcomeData[,outcomeCol]), na.rm = T)
+    
+    ## rate
+}
